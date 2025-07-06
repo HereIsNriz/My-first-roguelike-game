b991bb54bbc713d465dcc4179483aac567ffeaa1
Nriz <here.is.nriz@gmail.com>
2025-07-06 21:27:11 +0700

    Random Obstacle Spawn
    Obstacles will spawn randomly just after the game is started. Obstacles will not overlapping with other obstacles or with the walls.

--- My first roguelike game/Assets/Scripts/ObstacleSpawn.cs
+++ My first roguelike game/Assets/Scripts/ObstacleSpawn.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ObstacleSpawn : MonoBehaviour
 {
     public GameObject[] obstacles;
-
-    private int numOfObstacles = 20;
-    private float xySpawnLocation = 50.0f;
+    public int numOfObstacles = 10;
+    public float xySpawnLocation = 50.0f;
+    public float minDistanceBetweenObstacles = 1.0f;
+    
+    private List<Vector2> spawnedObstacleLocations = new List<Vector2>();
 
     // Start is called before the first frame update
     void Start()
@@ -15,22 +17,58 @@ public class ObstacleSpawn : MonoBehaviour
         ObstacleSpawnRandomly();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // Spawn Obstacles At Start Randomly
     void ObstacleSpawnRandomly()
     {
         for (int i = 0; i < numOfObstacles; i++)
         {
-            int obstacleIndex = Random.Range(0, obstacles.Length);
-            Vector2 obstacleLocation = new Vector2(Random.Range(-xySpawnLocation, xySpawnLocation), Random.Range(-xySpawnLocation, xySpawnLocation));
-            Quaternion obstacleRotation = Quaternion.Euler(0, 0, Random.Range(0, 360.0f));
+            Vector2 obstacleLocation = Vector2.zero;
+            bool locationFound = false;
+            int attemps = 0;
+            int maxAttemps = 100;
 
-            Instantiate(obstacles[obstacleIndex], obstacleLocation, obstacleRotation);
+            while (!locationFound && attemps < maxAttemps)
+            {
+                obstacleLocation = new Vector2(Random.Range(-xySpawnLocation, xySpawnLocation), Random.Range(-xySpawnLocation, xySpawnLocation));
+                locationFound = true;
+
+                foreach (Vector2 existingLocation in spawnedObstacleLocations)
+                {
+                    if (Vector2.Distance(obstacleLocation, existingLocation) < minDistanceBetweenObstacles)
+                    {
+                        locationFound = false;
+                        break;
+                    }
+                }
+
+                if (locationFound)
+                {
+                    Collider2D[] hitColliders = Physics2D.OverlapCircleAll(obstacleLocation, minDistanceBetweenObstacles / 2f);
+                    foreach (Collider2D hitCollider in hitColliders)
+                    {
+                        if (hitCollider.CompareTag("Obstacle") || hitCollider.CompareTag("Wall"))
+                        {
+                            locationFound = false;
+                            break;
+                        }
+                    }
+                }
+
+                attemps++;
+            }
+
+            if (locationFound)
+            {
+                int obstacleIndex = Random.Range(0, obstacles.Length);
+                Quaternion obstacleRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+
+                Instantiate(obstacles[obstacleIndex], obstacleLocation, obstacleRotation);
+                spawnedObstacleLocations.Add(obstacleLocation);
+            }
+            else
+            {
+                Debug.LogWarning($"Could not find a unique location for an obstacle after {maxAttemps} attemps.");
+            }
         }
     }
 }
