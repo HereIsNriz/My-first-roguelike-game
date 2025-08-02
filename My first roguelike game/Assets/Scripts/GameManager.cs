c74d30c7e47aef929b33eba4183b44da002b5bf1
Nriz <here.is.nriz@gmail.com>
2025-08-02 21:05:00 +0700

    Game Over to Stats

--- My first roguelike game/Assets/Scripts/GameManager.cs
+++ My first roguelike game/Assets/Scripts/GameManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public bool isGameRunning;
 
-    //
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject statsPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +27,17 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isGameRunning = false;
+        gameOverPanel.gameObject.SetActive(true);
+    }
+
+    public void NextButton()
+    {
+        gameOverPanel.gameObject.SetActive(false);
+        statsPanel.gameObject.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
