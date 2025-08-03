375434fc6e5d43c1b989f86772199f356209ce51
Nriz <here.is.nriz@gmail.com>
2025-08-03 19:42:35 +0700

    Menu UI
    Add Ui at the menu like title, play button, setting button (unfinish), and exit button.

---
+++ My first roguelike game/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void SettingButton()
+    {
+
+    }
+
+    public void ExitButton()
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
